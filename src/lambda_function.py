b4da05998c71b78e76772b5a25d5b040873824af
Verma <vanshverma@deloitte.com>
2023-03-31 20:31:31 +0530

    dynamoDB update

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -6,16 +6,12 @@ now = datetime.now()
 current_time =now.strftime("%H:%M:%s")
 
 def lambda_handler (event, context):
-    print("Hello SAM")
+    print("Hello!")
     print("Current Time is", current_time)
 
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from lambda')
     }
-
- 
-
- 
 
 
